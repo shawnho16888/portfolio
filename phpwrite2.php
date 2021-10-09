040f81829216bbc82b76c48d4a6f4d4a2bba3b37
shawnho16888 <91663897+shawnho16888@users.noreply.github.com>
2021-10-10 00:42:09 +0800

    Update phpwrite2.php

--- phpwrite2.php
+++ phpwrite2.php
@@ -2,7 +2,7 @@
   $myFile = "ttt.txt";
 
   $fh = fopen($myFile, 'w') or die("can't open file");
-  //$stringData = $_POST["name"];
+  $stringData = $_POST["name"];
   $stringData = "Hello World!";
   fwrite($fh, $stringData);
   fclose($fh);
