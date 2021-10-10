503fa002fc28883bcfcf655d66f16ab52aad3931
shawnho16888 <91663897+shawnho16888@users.noreply.github.com>
2021-10-10 22:14:11 +0800

    Delete phpwrite2.php

--- phpwrite2.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $myFile = "test.txt";
-
-  $fh = fopen($myFile, 'w') or die("can't open file");
-  $stringData = $_POST["name"];
-  $stringData = "Hello World!";
-  fwrite($fh, $stringData);
-  fclose($fh);
-?>  
